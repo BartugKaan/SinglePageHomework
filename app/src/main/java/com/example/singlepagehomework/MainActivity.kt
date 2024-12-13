2d0498975fe0898bde628458cd59af7067c5f44c
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-13 15:43:09 +0300

    add: About, Color and Add to cart button

--- app/src/main/java/com/example/singlepagehomework/MainActivity.kt
+++ app/src/main/java/com/example/singlepagehomework/MainActivity.kt
@@ -14,8 +14,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ShoppingCart
 import androidx.compose.material.icons.rounded.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,9 +29,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,6 +44,8 @@ import com.example.singlepagehomework.ui.theme.ButtonTextColor
 import com.example.singlepagehomework.ui.theme.ButtonTextColorDark
 import com.example.singlepagehomework.ui.theme.PrimaryTextColor
 import com.example.singlepagehomework.ui.theme.PrimaryTextColorDark
+import com.example.singlepagehomework.ui.theme.SecondaryTextColor
+import com.example.singlepagehomework.ui.theme.SecondaryTextColorDark
 import com.example.singlepagehomework.ui.theme.SinglePageHomeworkTheme
 import com.example.singlepagehomework.ui.theme.StarColor
 import com.example.singlepagehomework.ui.theme.StarColorDark
@@ -113,10 +122,62 @@ fun MainScreen(darktheme: Boolean = isSystemInDarkTheme()){
                     Text(" 4.1(620)", color = if (darktheme) ButtonBackGroundDark else ButtonBackGround)
                 }
             }
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 15.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text("About", fontSize = 20.sp)
+                Text(
+                    "JBL E55BT wireless over-ear headphones bring the signature sound of JBL right to your ears." +
+                        "The E55BT is one of our most versatile packages ever.",
+                    color = if (darktheme) SecondaryTextColorDark else SecondaryTextColor
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 15.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text("Color: ", fontSize = 20.sp, color = if (darktheme) PrimaryTextColorDark else PrimaryTextColor)
+                Row (
+                    modifier = Modifier.width(120.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                    ){
+                    ColorButton(Color.Blue)
+                    ColorButton(Color.Red)
+                    ColorButton(Color.Black)
+                }
+            }
+            Button(
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 15.dp),
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = if (darktheme) ButtonBackGroundDark else ButtonBackGround,
+                    contentColor = if (darktheme) ButtonTextColorDark else ButtonTextColor)
+                ) {
+                Text("Add to Cart", fontSize = 25.sp)
+                Spacer(modifier = Modifier.width(20.dp))
+                Icon(Icons.Rounded.ShoppingCart, contentDescription = "")
+            }
         }
     }
 }
 
+
+@Composable
+fun ColorButton(ButtonColor: Color){
+    Button(
+        modifier = Modifier.size(30.dp),
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(
+            containerColor = ButtonColor
+        )
+    ) {
+        Text("")
+    }
+}
 
 
 @Preview(showBackground = true)
