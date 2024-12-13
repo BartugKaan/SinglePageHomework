8ca7f0b0d4e3ce781d0ec2225d045abfbf4ae0ae
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-13 14:53:55 +0300

    refactor: sdk version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.singlepagehomework"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.singlepagehomework"
