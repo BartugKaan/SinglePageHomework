5cb5a6e8e98f05b0adf1998e63970aad642a7400
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-13 14:42:21 +0300

    add: TopAppBar and Main Column

--- app/src/main/java/com/example/singlepagehomework/MainActivity.kt
+++ app/src/main/java/com/example/singlepagehomework/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.singlepagehomework.ui.theme.ButtonBackGround
+import com.example.singlepagehomework.ui.theme.ButtonBackGroundDark
+import com.example.singlepagehomework.ui.theme.PrimaryTextColor
+import com.example.singlepagehomework.ui.theme.PrimaryTextColorDark
 import com.example.singlepagehomework.ui.theme.SinglePageHomeworkTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,11 +37,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(darktheme: Boolean = isSystemInDarkTheme()){
     val configuration = LocalConfiguration.current
     val screenHeight = configuration.screenHeightDp
+    val screenWidth = configuration.screenWidthDp
 
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(
+            title = { Text("JBL55BT") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = if(darktheme) ButtonBackGroundDark else ButtonBackGround,
+                titleContentColor = if (darktheme) PrimaryTextColorDark else PrimaryTextColor
+            ),
+        )
+    }) { paddingValues ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
 }
 
 
