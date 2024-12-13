41ecff7b543d72ff1543d26e706a772ab301156f
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-13 14:53:34 +0300

    add: Image and Custom Font

--- app/src/main/java/com/example/singlepagehomework/ui/theme/Type.kt
+++ app/src/main/java/com/example/singlepagehomework/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.singlepagehomework.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.singlepagehomework.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,6 @@ val Typography = Typography(
     )
     */
 )
+
+//Custom Font
+val kanit = FontFamily(Font(R.font.kanit))
